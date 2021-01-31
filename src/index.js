5c9cc9c23ce4ac184fa3a189808a8f8f91eedba4
Andrés Arana Castillo <aac.devs@gmail.com>
2021-01-30 22:14:29 -0300

    useContext practice done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import { Padre } from "./components/07-tarea-memo/Padre";
-import { TodoApp } from "./components/08-useReducer/TodoApp";
+// import { TodoApp } from "./components/08-useReducer/TodoApp";
+import { MainApp } from "./components/09-useContext/MainApp";
 // import { CallbackHook } from "./components/06-memos/CallbackHook";
 // import { MemoHook } from "./components/06-memos/MemoHook";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
@@ -16,4 +17,4 @@ import { TodoApp } from "./components/08-useReducer/TodoApp";
 // import { HookApp } from "./HookApp";
 // import "./components/08-useReducer/intro-reducers";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
